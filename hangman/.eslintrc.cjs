e3722d487c204b7863b4efabc609e0390c20786f
Anna Golosova <golosova76@gmail.com>
2024-01-09 12:45:17 +0300

    feat: add structure project (folder and file)

---
+++ hangman/.eslintrc.cjs
@@ -0,0 +1,56 @@
+const path = require('path');
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    // es6: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', path.resolve(__dirname, 'src')],
+          ['@images', path.resolve(__dirname, 'src/assets/image')],
+          ['@fonts', path.resolve(__dirname, 'src/assets/fonts')],
+          ['@styles', path.resolve(__dirname, 'src/styles')],
+        ],
+        extensions: ['.js', '.jsx', '.json', '.scss', '.html'],
+      },
+    },
+  },
+  plugins: ['prettier'],
+  noInlineConfig: true,
+};
---
+++ hangman/vite.config.js
@@ -0,0 +1,26 @@
+import Inspect from 'vite-plugin-inspect';
+import path from 'path';
+// import { resolve } from 'path'; убрать path отсюда path.resolve(__dirname, 'src'),
+
+export default {
+  build: {
+    minify: false,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        // Например страница "about.html" в папке "src/html"
+        about: path.resolve(__dirname, 'src/html/about.html'),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@images': path.resolve(__dirname, 'src/assets/image'),
+      '@fonts': path.resolve(__dirname, 'src/assets/fonts'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@public': path.resolve(__dirname, 'public'),
+    },
+  },
+  plugins: [Inspect()],
+};
