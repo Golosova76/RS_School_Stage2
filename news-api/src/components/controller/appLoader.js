f34ee058398bd4b1a574afd5cd0eab72fdc3e2d1
Anna Golosova <golosova76@gmail.com>
2024-02-25 21:15:38 +0300

    feat: set up ESLint and Webpack to work with TypeScript

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -1,8 +1,5 @@
 import Loader from './loader';
 
-const API_KEY = '775fca8df4be4549aa2f1bea062b444d';
-
-
 class AppLoader extends Loader {
     constructor() {
         super(process.env.API_URL, {
