64a2edd0a8068ab10f5b2f92c70f0439b0fd956f
Anna Golosova <golosova76@gmail.com>
2024-03-07 17:11:05 +0300

    chore: add Vite files and setting configurations (#40)
    * chore: add Vite files in project
    
    * chore: add Prettier and ESLint in Vite
    
    * chore: add Husky in Vite
    
    * fix: deletе the commas and version TS

---
+++ rss-puzzle/.eslintrc.cjs
@@ -0,0 +1,63 @@
+const path = require('path');
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    // es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', path.resolve(__dirname, 'src')],
+          ['@images', path.resolve(__dirname, 'src/assets/image')],
+          ['@fonts', path.resolve(__dirname, 'src/assets/fonts')],
+          ['@styles', path.resolve(__dirname, 'src/styles')],
+          ['@js', path.resolve(__dirname, 'src/js')],
+        ],
+        extensions: ['.js', '.jsx', '.json', '.scss', '.html', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  noInlineConfig: false,
+};
---
+++ rss-puzzle/validate-branch-name.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  pattern:
+    '^(feat|fix|docs|style|refactor|test|chore)/RSS-PZ-\\d{2}_[a-zA-Z0-9_-]+$',
+  errorMsg:
+    'Branch name does not match the rules specified in the repository for RSS Puzzle!',
+};
