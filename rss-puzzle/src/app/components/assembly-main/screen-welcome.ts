b0d06d64a3dd147e6150e5f4baee38ab35d11969
Anna Golosova <golosova76@gmail.com>
2024-03-14 12:14:50 +0300

    feat: add button start (#50)

--- rss-puzzle/src/app/components/assembly-main/screen-welcome.ts
+++ rss-puzzle/src/app/components/assembly-main/screen-welcome.ts
@@ -1,4 +1,5 @@
 import ArticleWelcomeComponent from '../article-welcome/article-welcome';
+import ButtonComponent from '../button/button';
 import GameUser from '../../services/user/local-storage';
 
 class WelcomeScreen {
@@ -10,12 +11,17 @@ class WelcomeScreen {
 
   // eslint-disable-next-line class-methods-use-this
   render(): HTMLElement {
+    const startButton = new ButtonComponent({
+      className: 'start-button',
+      text: 'Start',
+      type: 'submit',
+    });
     // Создание экземпляра article
     const articleWelcome = new ArticleWelcomeComponent();
-    // articleWelcome.appendToArticleWDirectly(startButton);
+    articleWelcome.appendToArticleWDirectly(startButton);
 
-    const articleWelcomeElement: HTMLFormElement =
-      articleWelcome.getNode() as HTMLFormElement;
+    const articleWelcomeElement: HTMLElement =
+      articleWelcome.getNode() as HTMLElement;
 
     // экземпляр GameUser, auto загружает данные из localStorage
     const gameUser = new GameUser();
@@ -23,6 +29,12 @@ class WelcomeScreen {
     const surname = gameUser.gameSurname ?? '';
 
     articleWelcome.setUserGreeting(name, surname);
+
+    startButton.addListener('click', (event) => {
+      event.preventDefault(); // Предотвращаем отправку формы, если это submit button
+      this.onLoginSuccess();
+      document.body.classList.remove('welcome-body'); // Вызов метода для перехода на следующий экран
+    });
 
     return articleWelcomeElement;
   }
