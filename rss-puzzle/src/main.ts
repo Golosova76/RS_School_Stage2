7dabb3f8ede017d83d2a2aa7972dd27d9298596a
Anna Golosova <golosova76@gmail.com>
2024-03-17 22:34:04 +0300

    Delete rss-puzzle directory

--- rss-puzzle/src/main.ts
+++
@@ -1,6 +0,0 @@
-import './styles/style.scss';
-
-import App from './app/app';
-
-// eslint-disable-next-line no-new
-new App();
--- rss-puzzle/validate-branch-name.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  pattern:
-    '^(feat|fix|docs|style|refactor|test|chore)/RSS-PZ-\\d{3}_[a-zA-Z0-9_-]+$',
-  errorMsg:
-    'Branch name does not match the rules specified in the repository for RSS Puzzle!',
-};
