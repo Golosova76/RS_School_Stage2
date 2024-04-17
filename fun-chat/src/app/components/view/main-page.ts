ba9d4655b80ca9d1321755c5cea5f62d7684dac3
Anna Golosova <golosova76@gmail.com>
2024-04-17 22:17:07 +0300

    feat: add main block container

--- fun-chat/src/app/components/view/main-page.ts
+++ fun-chat/src/app/components/view/main-page.ts
@@ -1,3 +1,26 @@
-class MainView {}
+// import Router from '../../utils/router';
+import { View } from '../model/common';
+
+class MainView implements View {
+  public container: HTMLDivElement;
+
+  constructor() {
+    this.container = document.createElement('div');
+    this.container.classList.add('main-container');
+    document.body.appendChild(this.container);
+    this.initializeContent();
+  }
+
+  private initializeContent(): void {
+    const mainShadow = document.createElement('div');
+    mainShadow.classList.add('shadow-main');
+    this.container.appendChild(mainShadow);
+
+    const title = document.createElement('h1');
+    title.textContent = 'Fun Chat';
+    title.classList.add('title-about');
+    mainShadow.appendChild(title);
+  }
+}
 
 export default MainView;
